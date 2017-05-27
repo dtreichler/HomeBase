219d3a6b1c3007aae18311887efc989d4f3bc3d6
Derrick Treichler <dtreichl@gmail.com>
2017-05-27 12:26:54 -0400

    PEP8 for .fonts

--- homebase/fonts/__init__.py
+++ homebase/fonts/__init__.py
@@ -8,16 +8,18 @@ DEFAULT_FONT = 'droidsans'
 
 logger = logging.getLogger('homebase.fonts')
 
+
 def get_module_fonts():
     dirname = os.path.dirname(__file__)
     fontfiles = glob.glob(os.path.join(dirname, '*.[ot]tf'))
     fonts = {}
     for f in fontfiles:
         filename = os.path.basename(f)
-        (root,ext) = os.path.splitext(filename)
+        (root, ext) = os.path.splitext(filename)
         root = root.lower()
         fonts[root] = f
     return fonts
+
 
 def get_system_fonts():
     sp = subprocess.Popen('fc-list', stdout=subprocess.PIPE)
@@ -26,10 +28,11 @@ def get_system_fonts():
     fonts = {}
     for f in fontfiles:
         filename = os.path.basename(f)
-        (root,ext) = os.path.splitext(filename)
+        (root, ext) = os.path.splitext(filename)
         root = root.lower()
         fonts[root] = f
     return fonts
+
 
 mod_fonts = get_module_fonts()
 sys_fonts = get_system_fonts()
@@ -37,13 +40,14 @@ sys_fonts = get_system_fonts()
 fonts = sys_fonts.copy()
 fonts.update(mod_fonts)
 
+
 def find_font(fontface):
     if os.path.isfile(fontface):
         fontfile = fontface
         logger.debug('Found font file {}'.format(fontfile))
     else:
         try:
-            (fontface,ext) = os.path.splitext(fontface)
+            (fontface, ext) = os.path.splitext(fontface)
             fontfile = fonts[fontface.lower()]
             logger.debug('Found font {}'.format(fontfile))
         except KeyError:
